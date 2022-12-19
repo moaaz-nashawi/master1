b4d563653d5d32e77133b17160325fe3ef3d1eef
Moaaz Nashawi <m.nashawi@anchormen.nl>
2022-12-19 14:47:49 +0100

    ss

---
+++ src/main/java/com/example/master1/Master1Application.java
@@ -0,0 +1,20 @@
+package com.example.master1;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Master1Application implements ApplicationRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Master1Application.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        //ClassOne classOne;
+        //
+    }
+}
---
+++ src/test/java/com/example/master1/Master1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.master1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Master1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
